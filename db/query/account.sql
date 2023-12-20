f7175197eb1942d3bc4daa971aa1e1f2c6477128
yeom-c <yjc720@gmail.com>
2023-12-20 15:37:27 +0900

    feat: add account balance

--- db/query/account.sql
+++ db/query/account.sql
@@ -14,6 +14,12 @@ SELECT *
 FROM accounts 
 WHERE id = $1;
 
+-- name: GetAccountForUpdate :one
+SELECT *
+FROM accounts
+WHERE id = $1
+FOR NO KEY UPDATE;
+
 -- name: ListAccounts :many
 SELECT * 
 FROM accounts 
@@ -24,6 +30,12 @@ LIMIT $1 OFFSET $2;
 UPDATE accounts
 SET balance = $2
 WHERE id = $1
+RETURNING *;
+
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
 RETURNING *;
 
 -- name: DeleteAccount :exec
